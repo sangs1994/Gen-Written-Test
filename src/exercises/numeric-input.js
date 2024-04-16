8f822baf7aa789badd23be5d70cb1ffaa987a78d
sangs1994 <sangeetha94.chandrasekar@gmail.com>
2024-04-16 21:21:03 +0100

    Numeric Value Validation
    1) Adding events
    2) CSS for input and msg

--- src/exercises/numeric-input.js
+++ src/exercises/numeric-input.js
@@ -18,11 +18,46 @@
 * red or green border to the input
 * */
 
+const numericValue = document.querySelector('.c-numeric-input');
+const errorMsg = document.querySelector('.c-numeric-input-error-msg');
+
 const NumericInput = {
   init: () => {
     document.querySelectorAll('.c-numeric-input').forEach(elem => {
       console.log('TODO: Please see the above requirement for numeric input');
     });
+  },
+  validateInput : (ev) =>{ 
+    const value = numericValue.value.trim();
+    const isValid = /^-?\d*\.?\d*$/.test(value);
+    if (!isValid) {
+      numericValue.classList.add('c-numeric-input-error');
+      numericValue.classList.remove('c-numeric-input-valid');
+      errorMsg.style.display = 'inline';
+    } else {
+      numericValue.classList.remove('c-numeric-input-error');
+      numericValue.classList.add('c-numeric-input-valid');
+      errorMsg.style.display = 'none';
+    }
+  },
+  focusOut : (ev) => {
+    ev.preventDefault();
+    console.log(ev.currentTarget.value);
+    numericValue.classList.remove('c-numeric-input-error', 'c-numeric-input-valid');
+    errorMsg.style.display = 'none';
+    numericValue.value =(numericValue.value &&!Number.isInteger(numericValue.value))  ? parseFloat(numericValue.value) : numericValue.value;
+  },
+  blurIn: (ev) =>{
+    ev.preventDefault();
+    const value = numericValue.value.trim();
+    const isValid = /^-?\d*\.?\d*$/.test(value);
+    if (isValid) {
+      numericValue.classList.add('c-numeric-input-valid');
+    } else {
+      numericValue.value = parseFloat(value) || '';
+    }
   }
+  
 };
 document.addEventListener('DOMContentLoaded', NumericInput.init);
+
